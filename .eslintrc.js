9fe394e9c7a5346479beec4177a7e952d66d88ce
jandersonp <jandersonpizziallyvilasboas@gmail.com>
2020-04-03 17:20:23 -0300

    Defindo PropTypes, Exibindo respositórios, Exibindo Issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,5 +28,6 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'no-console': ['error', { allow: ['tron'] }],
     'no-param-reassign': 'off',
+    'react/static-property-placement': 'off'
   },
 };
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { FaSpinner, FaArrowLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import Container from '../../components/Container';
+
+import { Loading, Owner, IssueList } from './styles';
 
 export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
   state = {
     repository: {},
     issues: [],
@@ -35,6 +48,43 @@ export default class Repository extends Component {
 
   render() {
     const { repository, issues, loading } = this.state;
-    return <h1>Repository</h1>;
+
+    if (loading) {
+      return (
+        <Loading loading={loading}>
+          <FaSpinner size={30} />
+        </Loading>
+      );
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">
+            <FaArrowLeft color="#191920" />
+          </Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <IssueList>
+          {issues.map((issue) => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map((label) => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
+    );
   }
 }
